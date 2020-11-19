a8a6e46565ee5b355123673c27757d7dc31580d7
Antonio Tomaz <antonio.tomaz@grupopmz.com.br>
2020-11-19 15:03:07 -0400

    rename class

--- src/main/java/com/arthech/produtocatalog/ProductCatalogApplication.java
+++ src/main/java/com/arthech/produtocatalog/ProductCatalogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdutoCatalogApplication {
+public class ProductCatalogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProdutoCatalogApplication.class, args);
+        SpringApplication.run(ProductCatalogApplication.class, args);
     }
 
 }
